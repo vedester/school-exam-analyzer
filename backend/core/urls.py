5916a15a768c9e6e3605578273d97789cfe4afb4
Vede Ster <vedester@gmail.com>
2025-11-25 21:51:36 +0300

    Fixing a media file for production purposes

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,10 +1,11 @@
 #backend/core/urls.py
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include,re_path
 from django.conf import settings
 
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,7 +13,11 @@ urlpatterns = [
     path('api/', include('analytics.urls')),
 
 
-    
+    # Add this block to serve media files in production
+
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
 
     
 ]
