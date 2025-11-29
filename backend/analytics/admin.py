480a638b0162d7c8d49d92518ead98dc10adf6a2
Vede Ster <vedester@gmail.com>
2025-11-29 12:47:10 +0300

    Show user in admin panel

--- backend/analytics/admin.py
+++ backend/analytics/admin.py
@@ -6,10 +6,12 @@ class ExamUploadAdmin(admin.ModelAdmin):
     # columns to show in the list view
     list_display = ('title', 'uploaded_at', 'status', 'message')
     # adds side filters for easy filtering
-    list_filter = ('status', 'uploaded_at')
+    list_filter = ('status', 'uploaded_at' ,'uploaded_by')
     # enables search box or bar to search by title or message
-    search_fields = ('title', 'message')
+    # You can now type a username or email in the search box to find their files
+    search_fields = ('title', 'message', 'uploaded_by__username', 'uploaded_by__email')
+    
      # Make the file read-only in admin so you don't accidentally change it
-    readonly_fields = ('uploaded_at',)
+    readonly_fields = ('uploaded_at','uploaded_by')
     # default ordering of records
     ordering = ('-uploaded_at',)
